25902207e9e44523a03fda4292999825d21c7493
Addou <Addou.ismail@gmail.com>
2015-04-08 13:41:55 +0100

    aaa

--- MarkExpress/MarkExpress/Form1.Designer.cs
+++ MarkExpress/MarkExpress/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(324, 126);
+            this.checkBox1.Location = new System.Drawing.Point(318, 57);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 0;
@@ -46,7 +46,7 @@
             // dataGridView1
             // 
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(144, 199);
+            this.dataGridView1.Location = new System.Drawing.Point(138, 130);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(240, 150);
             this.dataGridView1.TabIndex = 1;
