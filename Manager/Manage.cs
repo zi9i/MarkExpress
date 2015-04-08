a9b34a2b483d8530dd7490c99033ebc193fd64ac
Addou <Addou.ismail@gmail.com>
2015-04-08 18:27:07 +0100

    kkk

---
+++ Manager/Manage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Manager
+{
+    public class Manage
+    {
+        public GNoteEntities gn = new GNoteEntities();
+
+        public IEnumerable<Object> ListerNiveaux()
+        {
+            var requete = from niv in gn.Niveau select niv;
+            return requete;
+        }
+    }
+}
