67b23a5ca38096642269e439fdf4766db9a59cae
zi9o <zakariaelfakhry@gmail.com>
2015-04-09 14:27:46 +0100

    zqfdgh

---
+++ Manager/MatiereManager.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyEntityModel;
+
+namespace Manager
+{
+    public class MatiereManager :Manage
+    {
+        public IEnumerable<Object> ListerAllMatieres()
+        {
+            var req = from Mat in gn.Matiere.Include("Enseignant")
+                      where Mat.Matricule==Mat.Enseignant.Matricule
+                      select 
+                          new 
+                          {
+                              Désignation =Mat.NomMatiere,
+                              Responsable =Mat.Enseignant.Nom 
+                          };
+            return req;
+        }
+    }
+}
